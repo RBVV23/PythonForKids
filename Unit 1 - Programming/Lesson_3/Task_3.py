3c6207cf19a980e11e0880653d98100fd4ff020c
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-01-01 23:43:04 +0300

    0101 2343

---
+++ Unit 1 - Programming/Lesson_3/Task_3.py
@@ -0,0 +1,58 @@
+# Описание: Кеша много путешествует и записывает в блокнотик, в каких странах и городах он побывал.
+# И вот настал этот момент, когда блокнотик кончился. Кеша понимает, что мы живем в век современных
+# технологий и можно автоматизировать этот процесс. Поэтому придумал программу, которая бы добавляла
+# страну в посещенные вместе с городом, а если он был в этой стране, то добавлял бы город к списку
+# посещенных городов. А еще по кнопочке бы она могла выводить страну, в которой Кеша посетил больше
+# всего городов. Но сам он к сожалению не может сделать такую программу, давайте поможем ему с этим!
+#
+# Задание: Написать программу, которая будет по введенной стране и городу добавлять эти данные в
+# словарь следующим образом: если такая страна уже есть, то добавлять в список городов новый город
+# (если конечно же его еще нет). Если такой страны нет, то добавлять новый элемент в словарь.
+# А если Кеша введет команду favorite, то программа выведет название страны, у которой количество
+# посещенных городов больше всего.
+
+book = {}
+book['England']=['London']
+book['France']=['Paris']
+book['Germany']=['Munich', 'Frankfurt', 'Dresden']
+
+# print('keys: ',book.keys())
+# print('values: ', book.values())
+
+def open(book):
+    for str in book.keys():
+        print('Страна: ',str)
+        print('Города: ', book.get(str))
+        print()
+
+is_continue = 1
+
+while is_continue != 0:
+    print('Введите страну и город через пробел (или команду): ')
+    command=input(str())
+    command_list=command.split()
+    country=command_list[0]
+    city=command_list[len(command_list) - 1]
+
+    if command == 'favorite':
+        max_visits=1
+        for word in book.keys():
+            if len(book.get(word)) > max_visits:
+                max_visits=len(book.get(word))
+                max_country=word
+        print('Наиболее часто посещаемая страна: ', max_country)
+        print('Количество посещенных в ней городов:', max_visits)
+        print()
+    elif command == 'open':
+        open(book)
+    elif command == 'exit':
+        is_continue=0
+    elif book.get(country) == None:
+        book[country]=[city]
+    else:
+        city_list=book.get(country)
+        city_list.append(city)
+        book[country]=city_list
+
+print('Итоговый список стран и городов:')
+open(book)
