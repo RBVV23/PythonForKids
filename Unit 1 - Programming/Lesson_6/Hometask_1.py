eee975ce6da7355121e762481bd3a60a313cb45a
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-01-03 20:38:58 +0300

    0301 2038

--- Unit 1 - Programming/Lesson_6/Hometask_1.py
+++ Unit 1 - Programming/Lesson_6/Hometask_1.py
@@ -42,3 +42,28 @@ for word in columns:
 print('Всего не хватает ' + str(del_sum) + ' значений из ' + str(data.shape[0]*data.shape[1]) + ' (' + str(round(100*del_sum/(data.shape[0]*data.shape[1]), 1)) + ' %)')
 print('Отсутствующие значения по признакам: ')
 print(del_info)
+
+#3. Вывести статистику по числовым факторам, а также по категориальным
+
+#print(data.describe())
+numbers=data.describe()
+print('ЧИСЛОВЫЕ ФАКТОРЫ')
+for line in numbers:
+    print('Статистика по признаку "'+ str(line)+'":')
+    print('\t' + 'Среднее значение: ' + str(numbers[line][1]))
+    print('\t' + 'СКО: ' + str(numbers[line][2]))
+    print('\t' + 'Минимальное значение: ' + str(numbers[line][3]))
+    print('\t' + 'Максимальное значение: ' + str(numbers[line][7]))
+
+
+print('КАТЕГОРИАЛЬНЫЕ ФАКТОРЫ')
+print(data.describe(include=['object', 'bool']))
+categories=data.describe(include=['object', 'bool'])
+
+for line in categories:
+    print(line)
+    print('Статистика по признаку "'+ str(line) + '":')
+    print('\t' + 'Уникальных значений: ' + str(categories[line][1]))
+    print('\t' + 'Наиболее частое значение: ' + str(categories[line][2]))
+    print('\t' + 'Максимальная частота встречаемости: ' + str(categories[line][3]) + '\n')
+
