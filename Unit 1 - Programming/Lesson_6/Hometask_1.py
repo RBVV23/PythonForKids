794f8771a98d7f7cd6ec61938f060316859b6de7
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-01-03 20:28:37 +0300

    0301 2028

--- Unit 1 - Programming/Lesson_6/Hometask_1.py
+++ Unit 1 - Programming/Lesson_6/Hometask_1.py
@@ -17,3 +17,28 @@ pd.set_option('display.max_columns', 15)
 pd.set_option('display.max_rows', 20)
 # print(data)
 print(data.head())
+
+
+#1 Указать сколько есть признаков и информация о скольких пассажирах представлена в датасете
+print('Файл содержит информацию о '+ str(data.shape[0]) + ' пассажирах по ' + str(data.shape[1]) + ' признакам.')
+
+#2. Указать в каких столбцах пропущены данные и сколько
+columns=data.columns
+
+del_sum=0
+del_info={}
+for word in columns:
+    inf = data[word].value_counts()
+    print('Признак "' + word + '" принимает '  + str(len(inf)) +  ' значения(-й): ')
+    print(data[word].value_counts() )
+    num_del=data.shape[0] - sum(inf)
+    if num_del != 0:
+        del_info[word]=num_del
+    del_sum+=num_del
+    print( 'Содержит информацию о ' + str(sum(inf)) + ' пассажирах' + ' из ' + str(data.shape[0]) + '.' )
+    print( 'Пропущенных значений: ' + str(num_del) + ' из ' + str(data.shape[0]) + '.' )
+    print()
+
+print('Всего не хватает ' + str(del_sum) + ' значений из ' + str(data.shape[0]*data.shape[1]) + ' (' + str(round(100*del_sum/(data.shape[0]*data.shape[1]), 1)) + ' %)')
+print('Отсутствующие значения по признакам: ')
+print(del_info)
