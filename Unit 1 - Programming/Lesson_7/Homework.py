8e4b99ad943ab428904a632aed7995cf76ebec6d
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-01-05 00:03:29 +0300

    0501 0003

--- Unit 1 - Programming/Lesson_7/Homework.py
+++ Unit 1 - Programming/Lesson_7/Homework.py
@@ -20,13 +20,17 @@ pd.set_option('display.max_columns', data.shape[1])
 
 # print(data.head())
 
+
 # 1. Вычислить средний возраст выживших женщин из кабин 1 класса (pclass = 1)
+
 dataset = data[(data['sex'] == 'female') & (data['pclass'] == 1) & (data['survived'] == 1)]
 answer_1 = dataset['age'].mean()
 print(dataset.head())
 print('\n Средний возраст выживших пассажиров-женщин из 1-го класса:\t ' + str(answer_1) + '\n')
 
+
 # 2. Вычислить, сколько выжило детей из пассажиров 3 класса (pclass = 3)
+
 dataset = data[(data['who'] == 'child') & (data['pclass'] == 3)]
 answer_2 = dataset['age'].mean()
 print(dataset.head())
@@ -54,9 +58,11 @@ for line in columns:
 print(dataset.info())
 print()
 
+
 # 5. Создать датасет, состоящий из двух столбцов - возраст и транспортные расходы (age и fare).
 # При этом в датасете должны остаться только те пассажиры, которые являются женщинами и которые выжили.
 # После чего посчитать среднее по получившимся столбцам (за одну команду)
+
 new_columns = ['age', 'fare']
 dataset=data
 dataset=dataset[ (data['sex'] == 'female') & (data['survived'] == 1) ]
@@ -68,6 +74,7 @@ print(dataset.head())
 print(dataset.apply(np.mean, axis=0).head())
 print()
 
+
 # 6. Сделать факторизацию столбца пол (sex) с помощью функции map
 
 d_1 = {'male' : 1, 'female' : 0}
@@ -77,18 +84,23 @@ print(dataset.head())
 
 
 # 7. Сделать факторизацию столбца пол (who) с помощью функции replace
+
 d_2 = {'child' : 2, 'man' : 1, 'woman' : 0}
 dataset = dataset.replace({'who' : d_2})
 print(dataset.head())
 
+
 # 8. Сделать факторизацию остальных категориальных столбцов
+
 dataset = data
 categories = data.describe(include = ['object', 'bool'])
 for line in categories:
     dataset[line]=pd.factorize(dataset[line])[0]
 print(dataset.head())
 
+
 # 9. Посчитать средний возраст и средние транспортные расходы (age и fare) каждой категории пассажиров (дети, взрослые и женщины)
+
 data=pd.read_csv('titanic.csv')
 factors = ['age', 'fare']
 
