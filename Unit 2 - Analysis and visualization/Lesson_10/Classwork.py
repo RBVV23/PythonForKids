c9b166cebcaa3efcffa4caac6fe27f73b9c040a9
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-01-08 22:30:12 +0300

    0801 2230

--- Unit 2 - Analysis and visualization/Lesson_10/Classwork.py
+++ Unit 2 - Analysis and visualization/Lesson_10/Classwork.py
@@ -7,3 +7,27 @@ pd.set_option('display.max_columns', 100)
 pd.set_option('display.max_rows', 250)
 pd.set_option('display.width', 1000)
 
+
+data = pd.read_csv('FIFA18_Ultimate_Team_players_2.csv')
+
+
+df=data.head(1000)
+
+sns.boxplot(x='overall', data=df, orient='h')
+plt.show()
+
+top_leagues = df['league'].value_counts().sort_values(ascending=False).head(5).index.values
+# bottom_leagues = df['league'].value_counts().sort_values(ascending=True).head(5).index.values
+# print(bottom_leagues)
+sns.boxplot(y='league', x='overall', data=df[df['league'].isin(top_leagues)], orient='h')
+# sns.boxplot(y='league', x='overall', data=df[df['league'].isin(bottom_leagues)], orient='h')
+plt.show()
+
+df = pd.read_csv('clients.csv')
+print(df.shape)
+print(df.info())
+
+## print(df['state'].value_counts())
+print(df['churn'].value_counts())
+df['churn'].value_counts().plot(kind='bar')
+plt.show()
