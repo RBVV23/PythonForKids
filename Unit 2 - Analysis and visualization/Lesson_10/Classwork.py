03849a722df0db94ca8cfecafd3b750843a3315f
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-01-08 22:28:35 +0300

    0801 2228

---
+++ Unit 2 - Analysis and visualization/Lesson_10/Classwork.py
@@ -0,0 +1,9 @@
+import pandas as pd
+# import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+pd.set_option('display.max_columns', 100)
+pd.set_option('display.max_rows', 250)
+pd.set_option('display.width', 1000)
+
