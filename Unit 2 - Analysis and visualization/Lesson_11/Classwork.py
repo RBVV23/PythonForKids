cbe453e5d542061a09e4697c2b48ff326fdf228d
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-01-09 13:54:33 +0300

    0901 1354

--- Unit 2 - Analysis and visualization/Lesson_11/Classwork.py
+++ Unit 2 - Analysis and visualization/Lesson_11/Classwork.py
@@ -24,3 +24,25 @@ hist_columns = ['total night minutes', 'customer service calls',
 for column_name in hist_columns:
     df[column_name].plot(kind='hist', title=column_name)
     plt.show()
+
+
+for column_name in hist_columns:
+    sns.distplot(df[column_name], label=column_name)
+    plt.show()
+
+hist_columns = ['total night minutes', 'customer service calls',
+               'total day calls', 'total intl calls', 'total night calls', 'total intl minutes',
+               'account length', 'total eve calls', 'total eve minutes', 'number vmail messages',
+               'total day minutes', 'number vmail messages']
+
+for column in hist_columns:
+   sns.boxplot(y='churn', x=column, data=df, orient='h')
+   plt.show()
+
+print(df['customer service calls'].value_counts())
+
+sns.countplot(x='customer service calls', hue='churn', data=df)
+plt.show()
+sns.boxplot(y='churn', x='total day minutes', data=df, orient='h')
+plt.show()
+
