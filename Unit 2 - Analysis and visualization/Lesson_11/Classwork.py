64d2f1d39dda2d05f041b229955879bd5f2da151
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-01-09 13:53:06 +0300

    0901 1353

--- Unit 2 - Analysis and visualization/Lesson_11/Classwork.py
+++ Unit 2 - Analysis and visualization/Lesson_11/Classwork.py
@@ -8,3 +8,19 @@ pd.set_option('display.max_rows', 250)
 pd.set_option('display.width', 1000)
 
 
+df = pd.read_csv('clients.csv')
+
+df = df.drop(['total day charge', 'total night charge', 'total eve charge', 'total intl charge'], axis=1)
+corr_matrix = df.drop(['state', 'international plan', 'voice mail plan', 'area code'], axis=1).corr()
+## print(corr_matrix)
+sns.heatmap(corr_matrix)
+plt.show()
+
+hist_columns = ['total night minutes', 'customer service calls',
+               'total day calls', 'total intl calls', 'total night calls', 'total intl minutes',
+               'account length', 'total eve calls', 'total eve minutes', 'number vmail messages',
+               'total day minutes']
+
+for column_name in hist_columns:
+    df[column_name].plot(kind='hist', title=column_name)
+    plt.show()
