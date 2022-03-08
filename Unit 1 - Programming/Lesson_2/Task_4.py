2e499e2c974945f8b86dd3edc89fabbb3a481875
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-03-08 22:44:08 +0300

    0803 2244

--- Unit 1 - Programming/Lesson_2/Task_4.py
+++ Unit 1 - Programming/Lesson_2/Task_4.py
@@ -3,28 +3,27 @@
 # остаток от деления большего числа на меньшее, посчитать
 # квадратный корень из меньшего числа.
 
-import math as M
+import math as m
 from random import randint
 
 print("Генерируем 2 случайных число от 10 до 100...")
-A=randint(10,100)
-B=randint(10,100)
-flag=1
-while A==B or flag==1:
-    flag=0
+A = randint(10, 100)
+B = randint(10, 100)
+flag = 1
+while A == B or flag == 1:
+    flag = 0
     A = randint(10, 100)
     B = randint(10, 100)
-    if A<B:
+    if A < B:
         A = A + B
         B = A - 2 * B
         A = (A - B) // 2
         B = A + B
 
-print('A =',A)
-print('B =',B)
+print('A =', A)
+print('B =', B)
 print('A > B')
 
-print('A^B = ',A**B)
-print('Остаток от деления A на B = ',A%B)
-print('Корень из B = ',M.sqrt(B))
-
+print('A^B = ', A**B)
+print('Остаток от деления A на B = ', A % B)
+print('Корень из B = ', m.sqrt(B))
