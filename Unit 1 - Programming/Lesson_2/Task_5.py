842075cf1ca829972aae3a8b601bfd42525d4955
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-03-08 22:45:23 +0300

    0803 2245

--- Unit 1 - Programming/Lesson_2/Task_5.py
+++ Unit 1 - Programming/Lesson_2/Task_5.py
@@ -1,12 +1,12 @@
 # Создадим строку с фразой “Я у мамы программист” и
 # выведем ее на экран сначала в нормальном порядке, а затем в обратном
 
-string='Я у мамы программист'
+string = 'Я у мамы программист'
 
 print('Прямой порядок:')
 for i in range(len(string)):
     print(string[i])
 
 print('Обратный порядок:')
-for i in range(len(string)-1,-1,-1):
+for i in range(len(string)-1, -1, -1):
     print(string[i])
