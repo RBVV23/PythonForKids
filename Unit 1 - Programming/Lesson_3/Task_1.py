e18aec9b1841c27e77c6066d63cbd7cf4c7c1c97
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-03-08 22:53:01 +0300

    0803 2252

--- Unit 1 - Programming/Lesson_3/Task_1.py
+++ Unit 1 - Programming/Lesson_3/Task_1.py
@@ -1,14 +1,14 @@
 # Давайте создадим список чисел от 0 до 100, а затем сформируем
 # новый список из элементов первого списка, которые делятся на 3 и больше 15
 
-list = [i for i in range(0,101)]
+my_list = [i for i in range(0, 101)]
 
-newlist = [i for i in list if (i % 3==0) and (i>15) ]
+new_list = [i for i in my_list if (i % 3 == 0) and (i > 15)]
 
 print('Исходный список')
-print('list = ')
-print(list)
+print('my_list = ')
+print(my_list)
 print()
 print('Фильтрованный список')
-print('newlist = ')
-print(newlist)
+print('new_list = ')
+print(new_list)
