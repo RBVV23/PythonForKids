ebf146dd69daf1b5bdca096faea0d6fe0430b1b7
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-03-15 20:46:22 +0300

    1503 2046

--- Unit 2 - Analysis and visualization/Lesson_8/Homework.py
+++ Unit 2 - Analysis and visualization/Lesson_8/Homework.py
@@ -5,7 +5,7 @@
 
 
 import pandas as pd
-import numpy as np
+
 
 data = pd.read_csv('titanic.csv')
 
@@ -14,13 +14,13 @@ pd.set_option('display.max_columns', 15)
 print(data.head())
 
 # 1. Показать, что средний возраст взрослых пассажиров (мужчин и женщин) в первом классе выше, чем во 2 и 3
-print( data.pivot_table(['age'], ['sex', 'class'], aggfunc = 'mean'))
+print(data.pivot_table(['age'], ['sex', 'class'], aggfunc='mean'))
 
 # 2. Посмотреть, влияет ли класс пассажира (pclass) на его смертность
-print( pd.crosstab(data['class'], data['alive'], normalize=True, margins=True ))
+print(pd.crosstab(data['class'], data['alive'], normalize=True, margins=True))
 
 # 3. Посмотреть, влияет ли пол пассажира (sex) на его смертность
-print( pd.crosstab(data['sex'], data['alive'], normalize=True, margins=True ))
+print(pd.crosstab(data['sex'], data['alive'], normalize=True, margins=True))
 
 # 4. Посмотреть на зависимость среднего возраста и средних транспортных расходов и смерти
-print( data.pivot_table(['age', 'fare'], ['alive'], aggfunc = 'mean'))
+print(data.pivot_table(['age', 'fare'], ['alive'], aggfunc='mean'))
