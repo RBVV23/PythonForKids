0d81d504e1de49264bcd888d0a730f445f59424b
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-03-15 20:43:47 +0300

    1503 2043

--- Unit 2 - Analysis and visualization/Lesson_8/Classwork.py
+++ Unit 2 - Analysis and visualization/Lesson_8/Classwork.py
@@ -10,13 +10,13 @@ data['random_column'] = np.random.randint(1, 4, data.shape[0])
 print(data.head())
 # print( pd.crosstab(data['species'], data['random_column']) )
 # print( pd.crosstab(data['species'], data['random_column'], normalize = True) )
-print( pd.crosstab(data['species'], data['random_column'], normalize = True, margins = True) )
+print(pd.crosstab(data['species'], data['random_column'], normalize=True, margins=True))
 
 # print( data.pivot_table(['sepal_length', 'sepal_width'], ['species'], aggfunc='mean') )
 # print( data.pivot_table(['sepal_length', 'sepal_width'], ['petal_width'], aggfunc='max') )
 
 data['random_column'] = np.random.randint(1, 3, data.shape[0])
-print( data.pivot_table(['sepal_length', 'sepal_width'], ['species', 'random_column'], aggfunc='min') )
+print(data.pivot_table(['sepal_length', 'sepal_width'], ['species', 'random_column'], aggfunc='min'))
 data=data.drop(['random_column'], axis=1)
 # print(data)
 
@@ -33,6 +33,4 @@ print(col_3)
 data['is_died'] = np.concatenate((col_1, col_2, col_3), axis=0)
 
 print(pd.crosstab(data['species'], data['is_died'], margins=True))
-print( data.pivot_table(['petal_length', 'petal_width', 'sepal_length', 'sepal_width'], ['is_died'], aggfunc='mean') )
-# print( data.pivot_table(['petal_length', 'petal_width', 'sepal_length', 'sepal_width'], ['species'], aggfunc='mean') )
-# print( data.pivot_table(['petal_length', 'petal_width', 'sepal_length', 'sepal_width'], ['species', 'is_died'], aggfunc='mean') )
+print(data.pivot_table(['petal_length', 'petal_width', 'sepal_length', 'sepal_width'], ['is_died'], aggfunc='mean'))
