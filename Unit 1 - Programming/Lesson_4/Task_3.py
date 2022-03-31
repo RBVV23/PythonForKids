e64472048902324e24c390b78da45e7540429110
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-03-31 12:20:33 +0300

    3103 1220

--- Unit 1 - Programming/Lesson_4/Task_3.py
+++ Unit 1 - Programming/Lesson_4/Task_3.py
@@ -1,11 +1,11 @@
-#У нас уже есть машина! Но нам этого мало. Мы хотим еще и покорять дороги на мотоцикле!
-#В этом деле главное безопасность. Нам понадобится защитная экипировка: шлем, наколенники и налокотники.
-#Мы создадим классы для шлема, наколенников и налокотников.
+# У нас уже есть машина! Но нам этого мало. Мы хотим еще и покорять дороги на мотоцикле!
+# В этом деле главное безопасность. Нам понадобится защитная экипировка: шлем, наколенники и налокотники.
+# Мы создадим классы для шлема, наколенников и налокотников.
 # А затем класс защитной экипировки, в котором полями класса будут созданы шлем, наколенники и налокотники.
-#*Во всех классах используем метод который перекрашивает вещь, меняет характеристики (например цвет или модель).
+# *Во всех классах используем метод который перекрашивает вещь, меняет характеристики (например цвет или модель).
 
 
-class helmet:
+class Helmet:
     def __init__(self, model="base", color=""):
         self._model = model
         self.color = color
@@ -16,14 +16,17 @@ class helmet:
 
     def get_model(self):
         return self._model
+
     def set_model(self, new_model):
         self._model = new_model
         print("Model is successfuly changed")
 
     def tell(self):
-        print("Model of this helmet is", self._model)
-        print("This helmet is", self.color)
-class elbowpad:
+        print(f'Model of this Helmet is {self._model}')
+        print(f'This Helmet is {self.color}')
+
+
+class Elbowpad:
     def __init__(self, model="base", color="none"):
         self._model = model
         self.color = color
@@ -34,14 +37,17 @@ class elbowpad:
 
     def get_model(self):
         return self._model
+
     def set_model(self, new_model):
         self._model = new_model
-        print("Model is successfuly changed")
+        print("Model is successfully changed")
 
     def tell(self):
-        print("Model of this elbow pad is", self._model)
-        print("This elbow pad is", self.color)
-class kneepad:
+        print(f'Model of this elbow pad is {self._model}')
+        print(f'This elbow pad is {self.color}')
+
+
+class Kneepad:
     def __init__(self, model="base", color="none"):
         self._model = model
         self.color = color
@@ -52,53 +58,58 @@ class kneepad:
 
     def get_model(self):
         return self._model
+
     def set_model(self, new_model):
         self._model = new_model
-        print("Model is successfuly changed")
+        print("Model is successfully changed")
 
     def tell(self):
-        print("Model of this knee pad is", self._model)
-        print("This knee pad is", self.color)
+        print(f'Model of this knee pad is {self._model}')
+        print(f'This knee pad is {self.color}')
 
-class equipment:
-    def __init__(self, head=helmet(), knee=kneepad(), elbow=elbowpad()):
-        self.head=head
-        self.knee=knee
-        self.elbow=elbow
 
-    def head_on(self, new_head=helmet()):
-        self.head=new_head
-    def knee_on(self, new_knee=kneepad()):
-        self.knee=new_knee
-    def elbow_on(self, new_elbow=elbowpad()):
-        self.elbow=new_elbow
+class Equipment:
+    def __init__(self, head=Helmet(), knee=Kneepad(), elbow=Elbowpad()):
+        self.head = head
+        self.knee = knee
+        self.elbow = elbow
+
+    def head_on(self, new_head=Helmet()):
+        self.head = new_head
+
+    def knee_on(self, new_knee=Kneepad()):
+        self.knee = new_knee
+
+    def elbow_on(self, new_elbow=Elbowpad()):
+        self.elbow = new_elbow
 
     def tell(self):
-        print("This equipment includes:")
+        print("This Equipment includes:")
         self.head.tell()
         self.knee.tell()
         self.elbow.tell()
 
-my_helmet = helmet()
-my_color="red"
-my_model="progressive"
-your_helmet = helmet("oldfashion", "blue")
-your_kneepad = kneepad("oldfashion", "blue")
-your_elbowpad = elbowpad("oldfashion", "blue")
+
+my_helmet = Helmet()
+my_color = "red"
+my_model = "progressive"
+your_helmet = Helmet("oldfashion", "blue")
+your_kneepad = Kneepad("oldfashion", "blue")
+your_elbowpad = Elbowpad("oldfashion", "blue")
 
 your_helmet.tell()
 my_helmet.repaint(my_color)
 my_helmet.set_model(my_model)
 
-my_elbowpad=elbowpad("safety", "red")
+my_elbowpad = Elbowpad("safety", "red")
 my_elbowpad.tell()
 
-my_kneepad=kneepad("safety", "red")
+my_kneepad = Kneepad("safety", "red")
 my_kneepad.tell()
 
 print("________________________________________")
 
-my_equipment=equipment(my_helmet, my_kneepad, my_elbowpad)
+my_equipment = Equipment(my_helmet, my_kneepad, my_elbowpad)
 my_equipment.tell()
 
 my_equipment.head_on(your_helmet)
