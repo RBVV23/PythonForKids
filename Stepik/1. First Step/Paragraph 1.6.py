53aa642c8e33989bfbea6db7c31f89226dfb208f
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2023-07-08 12:51:24 +0300

    0807 1251

--- Stepik/1. First Step/Paragraph 1.6.py
+++ Stepik/1. First Step/Paragraph 1.6.py
@@ -10,3 +10,15 @@ print(round(2.5))
 # >>> 2
 print(round(-2.5))
 # >>> -2
+
+
+# ---------------------------------------------
+
+
+# put your python code here
+from math import ceil
+
+x = int(input())
+y = int(input())
+
+print(ceil(y / x))
