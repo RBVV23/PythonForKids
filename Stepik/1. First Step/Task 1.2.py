c634c5ddde17a926ee42a506a2d4d06db3266e60
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2023-07-10 21:08:06 +0300

    1007 2107

---
+++ Stepik/1. First Step/Task 1.2.py
@@ -0,0 +1,10 @@
+#python 3.6.9
+'''
+Прежде чем запустить программу,
+нажмите на кнопку Show input и заранее введите входные данные!
+'''
+a = int(input())
+b = input()
+print ('a * "b" = {}'.format(a * b))
+print ('a * b = {}'.format(a * int(b)))
+print ('a * "b" = {0}; a * b = {1}'.format(format(a * b), a * int(b)))
