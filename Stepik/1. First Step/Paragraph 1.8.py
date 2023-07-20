78f7504cea3b505b3c612993c5a52ea1f2fc9606
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2023-07-20 13:09:27 +0300

    2007 1309

--- Stepik/1. First Step/Paragraph 1.8.py
+++ Stepik/1. First Step/Paragraph 1.8.py
@@ -22,16 +22,16 @@ print(s.split())
 # >>> ['one', 'two', 'three']
 
 
->>> a, b, c = "1 2 3".split()
->>> a
-'1'
->>> int(a)
-1
->>> b
-'2'
->>> int(b)
-2
->>> c
-'3'
->>> int(c)
-3
+a, b, c = "1 2 3".split()
+print(a)
+# >>> '1'
+print(int(a))
+# >>> 1
+print(b)
+# >>> '2'
+print(int(b))
+# >>> 2
+print(c)
+# >>> '3'
+print(int(c))
+# >>> 3
