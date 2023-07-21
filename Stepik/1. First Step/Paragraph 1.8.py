299bfd331cf827a15f93be75c0d14f8c085b6628
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2023-07-21 17:05:15 +0300

    2107 1705

--- Stepik/1. First Step/Paragraph 1.8.py
+++ Stepik/1. First Step/Paragraph 1.8.py
@@ -137,3 +137,9 @@ else:
 # ---------------------------------------------
 
 
+x1, y1 = map(int, input().split())
+x2, y2 = map(int, input().split())
+if (x1 * x2 <= 0) or (y1 * y2 <= 0):
+    print('YES')
+else:
+    print('NO')
