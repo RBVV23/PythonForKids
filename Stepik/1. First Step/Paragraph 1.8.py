795b016941409f4a81b947dcb77812218c64a262
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2023-07-22 16:44:19 +0300

    2207 1644

--- Stepik/1. First Step/Paragraph 1.8.py
+++ Stepik/1. First Step/Paragraph 1.8.py
@@ -101,3 +101,11 @@ print(c)  # 10
 
 a, b = map(int, input().split())
 print(a + b)
+
+
+
+x1, y1, x2, y2 = map(int, input().split())
+if (x1 == x2) or (y1 == y2):
+    print('YES')
+else:
+    print('NO')
